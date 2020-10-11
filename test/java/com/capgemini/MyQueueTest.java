bb74ad34b676d401cf62cc41d274c69d7c5b3f92
n-ndini7 <hp@RJs-PC>
2020-10-11 13:30:46 +0530

    [nandini] added UC4 - dequeue from the beginning

--- test/java/com/capgemini/MyQueueTest.java
+++ test/java/com/capgemini/MyQueueTest.java
@@ -18,4 +18,18 @@ public class MyQueueTest {
 		int size = queue.size();
 		Assert.assertEquals(3, size);
 	}
+
+	@Test
+	public void deQueueTestForQueue() {
+		MyQueue queue = new MyQueue();
+		Node<Integer> firstNode = new Node<>(70);
+		Node<Integer> secondNode = new Node<>(30);
+		Node<Integer> thirdNode = new Node<>(56);
+		queue.enQueue(firstNode);
+		queue.enQueue(secondNode);
+		queue.enQueue(thirdNode);
+		INode<Integer> tempNode = queue.deQueue();
+		queue.printQueue();
+		Assert.assertEquals(firstNode, tempNode);
+	}
 }
